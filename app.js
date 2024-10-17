67739b1746035ec3133ed02ec536ad0abee694df
LUCAS SANTOS FERREIRA <107937059+lucassantosferreira@users.noreply.github.com>
2024-10-17 10:51:19 -0300

    l

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-    cartao.innerHTML =
+
+cartao.innerHTML=`
     <div class="cartao_conteudo">
     <h3>${categoria}</h3>
     <div class="cartao_conteudo_pergunta"> <p>${pergunta}</p>
@@ -10,6 +11,8 @@ function criaCartao(categoria, pergunta, resposta) {
     <div class="cartao_conteudo_resposta"> <p>${resposta}</p>
     </div>
     </div>
+`
+
     let respostaEstaVisivel = false
     function viraCartao() {
     respostaEstaVisivel = !respostaEstaVisivel
